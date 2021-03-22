a9a608b26adfad3b8630789ba21aa2edf39fc8bf
oersoy91 <osman_ersoy@live.de>
2021-03-22 15:55:17 +0100

    🚚  Move types to types.ts

--- server/db.ts
+++ server/db.ts
@@ -7,30 +7,6 @@ export type PersonnelDoc = {
   age: number;
 };
 
-export type Person = {
-  _id: string;
-  profile_picture: string;
-  first_name: string;
-  last_name: string;
-  age: number;
-  birthday: string;
-  gender: string;
-  adress: Adress;
-  email: string;
-  start_contract: string;
-  end_contract: string;
-  end_trial_period: string;
-  position: string;
-  status: string;
-};
-
-export type Adress = {
-  street: string;
-  house_number: number;
-  zip_code: number;
-  city: string;
-};
-
 let client = null;
 let db = null;
 
