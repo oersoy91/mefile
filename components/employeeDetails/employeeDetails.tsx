810792945c177b827735d632114ab52d79683327
oersoy91 <osman_ersoy@live.de>
2021-03-25 21:24:52 +0100

    ✨ Add employee detail page

---
+++ components/employeeDetails/employeeDetails.tsx
@@ -0,0 +1,18 @@
+import { Person } from "../../utils/types";
+import styles from "./employeeDetails.module.css";
+
+export type EmployeeDetailProps = {
+  persons: Person;
+};
+const EmployeeDetails = ({ persons }: EmployeeDetailProps) => {
+  return (
+    <div className={styles.container}>
+      <div>{persons.last_name}</div>
+      <div>{persons.first_name}</div>
+      <div>{persons._id}</div>
+      <div>{persons.age}</div>
+    </div>
+  );
+};
+
+export default EmployeeDetails;
