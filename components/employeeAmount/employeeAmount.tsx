fddf03977860d0fb8566da4399bd117ef988ebee
oersoy91 <osman_ersoy@live.de>
2021-04-07 12:55:42 +0200

    ✨  Add SWR into employee amount component

--- components/employeeAmount/employeeAmount.tsx
+++ components/employeeAmount/employeeAmount.tsx
@@ -1,26 +1,44 @@
 import styles from "./employeeAmount.module.css";
 import Charts from "../charts/charts";
+import useSWR from "swr";
+import LoadingSpinner from "../loadingSpinner/loadingSpinner";
+
+export const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function EmployeeAmount() {
+  const { data: gender, error } = useSWR("/api/genderAmount", fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!gender)
+    return (
+      <div className={styles.container}>
+        <LoadingSpinner />
+      </div>
+    );
+
+  const male = gender[0].männlich;
+  const female = gender[0].weiblich;
+  const divers = gender[0].diverse;
+  const total = gender[0].total;
   return (
     <div>
       <div className={styles.container}>
         <h2 className={styles.header}>Mitarbeiterzahl</h2>
         <div className={styles.genderContainer}>
           <div className={styles.genderAmountContainer}>
-            <p className={styles.amount}>10</p>
+            <p className={styles.amount}>{total}</p>
             <p>Gesamt</p>
           </div>
           <div className={styles.genderAmountContainer}>
-            <p className={styles.amount}>10</p>
+            <p className={styles.amount}>{male}</p>
             <p>Männlich</p>
           </div>
           <div className={styles.genderAmountContainer}>
-            <p className={styles.amount}>10</p>
+            <p className={styles.amount}>{female}</p>
             <p>Weiblich</p>
           </div>
           <div className={styles.genderAmountContainer}>
-            <p className={styles.amount}>10</p>
+            <p className={styles.amount}>{divers}</p>
             <p>Diverse</p>
           </div>
         </div>
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Cal from "../components/calendar/calendar";
-import Charts from "../components/charts/charts";
 import EmployeeAmount from "../components/employeeAmount/employeeAmount";
 
 export default function Home() {
